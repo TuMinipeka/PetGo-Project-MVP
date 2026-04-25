d1576f0f26cae27fac39a4c08748c5c7a6085e6d
TuMinipeka <danielsantiagomayorga03@gmail.com>
2026-04-25 15:43:51 -0500

    feat: :sparkles: AddIconAssets

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,14 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import backgroundImage from '../assets/fondo.png';
+import icon from '../assets/LogoPetGo.png';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
+      <Image source={icon} style={styles.logo}/>
+      {/* Ahora styles.title sí existe */}
       <Text style={styles.title}>PetGo</Text>
+      <Text style={styles.text}>Respeto, amor y cuidado.</Text>
     </View>
   );
 }
@@ -13,9 +18,20 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    experimental_backgroundImage: backgroundImage
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 20, // Un poco de espacio extra
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
+    color: 'white',
   },
+  text: {
+    color: 'white',
+    marginBottom:`100px`, // Un poco de espacio extra
+  }
 });
